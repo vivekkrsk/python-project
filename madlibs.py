9e4fd86df6635c2f1de8e439b95f376634a5a570
vivekkrsk <86180566+vivekkrsk@users.noreply.github.com>
2022-09-01 12:18:45 +0530

    commit

---
+++ madlibs.py
@@ -0,0 +1,9 @@
+adj=input("Adjective: ")
+verb1 = input("verb: ")
+verb2 = input("verb: ")
+famous_person = input("famous person: ")
+
+madlib = f"Computer programing is so {adj}! It make me so excited all the time because \
+I love to {verb1}. Stay hyderated and {verb2} like you are {famous_person}!"
+
+print(madlib)
