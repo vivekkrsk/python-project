0fe72dfaf75e5d60a2d821b9a5c508e4bf38ab65
vivekkrsk <86180566+vivekkrsk@users.noreply.github.com>
2022-09-06 19:34:59 +0530

    commit

---
+++ turtle_pattern_05.py
@@ -0,0 +1,14 @@
+import turtle
+
+colors = ['red','purple','blue','green','orange','yellow']
+t=turtle.Pen()
+turtle.bgcolor("black")
+t.speed(0)
+
+for i in range(360):
+    t.pencolor(colors[i%6])
+    t.width(i//100+1)
+    t.forward(i)
+    t.left(59)
+turtle.hideturtle()
+turtle.done()
